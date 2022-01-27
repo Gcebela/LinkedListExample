2dbb99420d5412abe53d0ab0af2d10987f40ec43
Gcebela <gcebelashabangu07@gmail.com>
2022-01-27 21:39:17 +0200

    first commit

---
+++ MyList.h
@@ -0,0 +1,17 @@
+#pragma once
+
+typedef  char* DataType;
+
+struct node 
+{ DataType data;
+  node* next;
+};
+
+typedef  node*  List;
+           
+void Init  (List&);
+bool IsEmpty (List );
+void  Insert (DataType, List&);
+void  Delete (DataType, List&); 
+void  ShowData (List);
+//qw
